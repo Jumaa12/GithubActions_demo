563f97709de1914416385a39da356d73e76b4e0f
mungunsi <mobednfandikup@gmail.com>
2023-11-18 22:33:37 -0600

    tf workflow 13

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 terraform {
-  required_version = "~> 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "5.26.0"
     }
   }
 }
 
 # Provider Block
-provider "aws": {
+provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
